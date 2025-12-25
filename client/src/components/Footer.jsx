dc1784fbc51876a3dae48c665d688ecf7ddc0568
shubh4m1863 <160105402+shubh4m1863@users.noreply.github.com>
2025-12-25 12:52:13 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,13 +4,28 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
-       <img src={assets.logo} alt="" width={150} />
-       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @RCS.dev    |     All rights reserved.</p>
-       <div className='flex gap-2.5'>
-        <img src={assets.facebook_icon} alt="" width={35}/>
-        <img src={assets.twitter_icon} alt="" width={35}/>
-        <img src={assets.instagram_icon} alt="" width={35}/>
-       </div>
+      
+      <img src={assets.logo} alt="Logo" width={150} />
+
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
+        Copyright @RCS.dev | All rights reserved.
+      </p>
+
+      <div className='flex gap-2.5'>
+        <a
+          href="https://www.instagram.com/shubham4mm_88"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={assets.instagram_icon}
+            alt="Instagram"
+            width={35}
+            className="cursor-pointer hover:opacity-80"
+          />
+        </a>
+      </div>
+
     </div>
   )
 }
