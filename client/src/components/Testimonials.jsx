664169b8b5f865013f7839ad8434ab7896f27782
shubh4m1863 <160105402+shubh4m1863@users.noreply.github.com>
2025-12-25 12:59:47 +0530

    Update Testimonials.jsx

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,38 +1,33 @@
 import React from 'react'
-import { assets, testimonialsData } from '../assets/assets'
-import { motion } from "motion/react"
+import { assets } from '../assets/assets'
 
-const Testimonials = () => {
+const Footer = () => {
   return (
-    <motion.div 
-     initial={{opacity:0.2, y:100}} 
-    transition={{duration:1}}
-     whileInView={{opacity : 1 , y:0}}
-     viewport={{ once : true}}
-    className='flex flex-col items-center justify-center my-20 py-12 '>
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+      
+      <img src={assets.logo} alt="Logo" width={150} />
 
-         <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer Testimonials</h1>
-        <p className='text-gray-500 mb-8'>What Are Our User Are Saying</p>
-        <div className='flex flex-wrap gap-6'>
-         {testimonialsData.map((testimonial,index)=>(
-            <div key={index} className='bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all'>
-                <div className='flex flex-col items-center'>
-                    <img src={testimonial.image} alt="" className='rounded-full w-14' />
-                    <h2 className='text-xl font-semibold mt-3'>{testimonial.name}</h2>
-                    <p className='text-gray-500 mb-4'>{testimonial.role}</p>
-                    <div className='=flex mb-4'> 
-                      {Array(testimonial.stars).fill().map((item,index)=>(
-                        <img key={index} src={assets.rating_star} alt="" />
-                    ))}
-                    </div>
-                    <p className='text-center text-sm text-gray-600'>{testimonial.text}</p>
-                </div>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
+        Copyright @RCS.dev | All rights reserved.
+      </p>
 
-            </div>
-         ))}  
-        </div>
-    </motion.div>
+      <div className='flex gap-2.5'>
+        <a
+          href="https://www.instagram.com/shubham4mm_88"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={assets.instagram_icon}
+            alt="Instagram"
+            width={35}
+            className="cursor-pointer hover:opacity-80"
+          />
+        </a>
+      </div>
+
+    </div>
   )
 }
 
-export default Testimonials
+export default Footer
